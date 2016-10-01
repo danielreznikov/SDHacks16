35771e422fda7b8ff8e56b3fc1269a54e62f0cc0
gkrulce <gkrulce@gmail.com>
2016-10-01 12:58:45 -0700

    Implemented pick ups.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        
+        // Deactivating Pick Up object after collision.
+        if (other.gameObject.CompareTag ("Pick Up")) {
+            other.gameObject.SetActive (false);
+        }
+
+
+
+
+        Destroy(other.gameObject);
+    }
+}
