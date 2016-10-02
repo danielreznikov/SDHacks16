98108b30d6cb48e3075927879314b12ae0238e3a
danielreznikov <daniel.reznikov@gmail.com>
2016-10-02 10:31:15 -0700

    orbs

---
+++ Assets/Prefabs/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int collected = 0;
+	GUIText gui;
+
+	// Use this for initialization
+	void Start () {
+		gui = GetComponent<GUIText> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        
+        // Deactivating Pick Up object after collision.
+        if (other.gameObject.CompareTag ("Pick Up")) {
+            other.gameObject.SetActive (false);
+			collected += 1;
+        }
+
+		gui.text = "Collected " + collected + "/10";
+
+    }
+}
---
+++ Assets/Standard Assets/DisplayScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+	Text txt;
+	private int score = 0;
+
+	void Start() {
+		txt = gameObject.GetComponent<Text> ();
+		txt.text = "Items collected: " + score;
+	}
+
+	void Update() {
+		txt.text = "Items collected: " + score;
+		score = PlayerPrefs.GetInt ("SCORE");
+	}
+}
